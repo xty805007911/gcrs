2cb2d8098444743022245cc07689f123d7909771
肖天宇 <5180179+pastthewind@user.noreply.gitee.com>
2020-12-27 20:18:53 +0800

    用户定点完成

---
+++ src/main/java/com/ctsi/controller/OrderRestController.java
@@ -0,0 +1,12 @@
+package com.ctsi.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Description:
+ * @Author: Tianyu Xiao
+ * @CreateDate: 2020/12/27  19:40
+ */
+@RestController
+public class OrderRestController {
+}
